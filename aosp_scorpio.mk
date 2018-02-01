0af8ecb5f2ab867cff20b761322fc9de6e74b3dc
Bruno Martins <bgcngm@gmail.com>
2018-02-01 22:04:45 +0100

    scorpio: Set matching build fingerprint for system, vendor and boot images
    Change-Id: I555c15eddc6dff694285ab1b23f6f9adfd64e535

--- aosp_scorpio.mk
+++ aosp_scorpio.mk
@@ -14,5 +14,7 @@ PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 PRODUCT_BUILD_PROP_OVERRIDES += \
     TARGET_DEVICE="scorpio" \
     PRODUCT_NAME="scorpio" \
-    BUILD_FINGERPRINT="Xiaomi/scorpio/scorpio:7.0/NRD90M/V9.1.3.0.NADCNEI:user/release-keys" \
     PRIVATE_BUILD_DESC="scorpio-user 7.0 NRD90M V9.1.3.0.NADCNEI release-keys"
+
+# Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
+BUILD_FINGERPRINT := "Xiaomi/scorpio/scorpio:7.0/NRD90M/V9.1.3.0.NADCNEI:user/release-keys"
