3be7e5fb2bddc4e6883067dc8d5364e57a13378d
PsyMan47 <xhonirexhepi@gmail.com>
2018-04-25 01:36:37 +0000

    scorpio: Update build fingerprint to v9.5.2.0 China Stable

--- aosp_scorpio.mk
+++ aosp_scorpio.mk
@@ -14,7 +14,7 @@ PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 PRODUCT_BUILD_PROP_OVERRIDES += \
     TARGET_DEVICE="scorpio" \
     PRODUCT_NAME="scorpio" \
-    PRIVATE_BUILD_DESC="scorpio-user 7.0 NRD90M V9.1.3.0.NADCNEI release-keys"
+    PRIVATE_BUILD_DESC="scorpio-user 7.0 NRD90M V9.5.2.0.NADCNFA release-keys"
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
-BUILD_FINGERPRINT := "Xiaomi/scorpio/scorpio:7.0/NRD90M/V9.1.3.0.NADCNEI:user/release-keys"
+BUILD_FINGERPRINT := "Xiaomi/scorpio/scorpio:7.0/NRD90M/V9.5.2.0.NADCNFA:user/release-keys"
